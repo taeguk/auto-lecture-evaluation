63b6df731708f14ec1e240e39d9486865a1266c8
juice500ml <juice500ml@gmail.com>
2015-12-29 18:22:17 +0900

    exception: lowest>highest score

--- options.js
+++ options.js
@@ -3,6 +3,8 @@ function save_options() {
   var tellstring = document.getElementById('tellstring').value;
   var lowest = document.getElementById('lowest').value;
   var highest = document.getElementById('highest').value;
+  if(lowest>highest) highest = [lowest, lowest = highest][0];
+ 
   chrome.storage.sync.set({
     tellstring: tellstring,
     lowest: lowest,
